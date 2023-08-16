5119c5edcf84695bc28aa56a6cff891ecc2153fe
AnnaZaitsava <anya.polden@gmail.com>
2023-08-16 04:08:44 +0300

    Initial Commit

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Anna Zaitsava on 16.08.23.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet var billTextField: UITextField!
+    @IBOutlet var zeroPctButton: UIButton!
+    @IBOutlet var tenPctButton: UIButton!
+    @IBOutlet var twentyPctButton: UIButton!
+    @IBOutlet var splitNumberLabel: UILabel!
+    
+
+    @IBAction func tipChanged(_ sender: Any) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: Any) {
+    }
+}
+
