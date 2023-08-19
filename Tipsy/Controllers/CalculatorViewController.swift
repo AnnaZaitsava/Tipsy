4470daf31d538acef9773317e971a9d485bf1c16
AnnaZaitsava <anya.polden@gmail.com>
2023-08-19 18:49:21 +0300

    - add logic and segue

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -15,14 +15,59 @@ class CalculatorViewController: UIViewController {
     @IBOutlet var twentyPctButton: UIButton!
     @IBOutlet var splitNumberLabel: UILabel!
     
-
-    @IBAction func tipChanged(_ sender: Any) {
-    }
+    var tipAmount = 0.00
+    var splitBetweenNumber = 2
+    var billTotalAmount = 0.0
+    var result = ""
+    var tip = ""
     
-    @IBAction func stepperValueChanged(_ sender: Any) {
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+        billTextField.endEditing(true)
+        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        
+        tip = sender.currentTitle!
+        
+        let selectedTipAmount = Double(tip.dropLast())
+        
+        tipAmount = selectedTipAmount!  / 100
+        
+        }
+            
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(Int(sender.value))
+        splitBetweenNumber = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: Any) {
+        
+        let bill = billTextField.text!
+        if billTextField != nil {
+            billTotalAmount = Double(bill)!
+        }
+       
+        let calculatedBill =  Double(((billTotalAmount * tipAmount)
+        + billTotalAmount)  / Double(splitBetweenNumber))
+        
+        result =  String(format: "%.2f", calculatedBill)
+        
+        
+        self.performSegue(withIdentifier: "showResult", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.settingProperty = "Split between \(splitBetweenNumber) people, with \(tip) tip."
+            destinationVC.totalValue = result
+        }
     }
 }
 
--- Tipsy/Controllers/ResultViewController.swift
+++ Tipsy/Controllers/ResultViewController.swift
@@ -8,22 +8,24 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    var totalValue: String?
+    var settingProperty: String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        totalLabel.text = totalValue
+        settingsLabel.text = settingProperty
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true)
     }
-    */
 
 }
